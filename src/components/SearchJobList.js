883395d145e71364c9bdd7e3169252da96856aed
david espinal <despinal0425@gmail.com>
2021-03-15 10:56:03 -0400

    added the useStates to select the first ten items from the array

--- src/components/SearchJobList.js
+++ src/components/SearchJobList.js
@@ -1,31 +1,47 @@
-
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 
 //Post info goes here
 export default function SearchJobList() {
-  const [page, setPage] = useState(1);
 
+  //list of jobs
+  const jobsRequest = useSelector((state) => state.searches.searchJobs);
+  const loading = useSelector((state) => state.searches.status)
 
-  const jobRequest = useSelector((state) => state.searches.searchJobs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  
 
-  if (!jobRequest) {
-    return null;
+  if (!jobsRequest) { 
+    return null
   }
+  
+  const currentPosts = jobsRequest.slice(indexOfFirstPost, indexOfLastPost);
+  console.log(currentPosts)
 
-  console.log(`There are ${jobRequest.length} jobs!`);
+  
 
   function renderList() {
-    return jobRequest.map((post, index) => {
-      return (
-        <ul>
-          <li key={index}>{post.company_name}</li>
-        </ul>
-      );
-    });
-  }
 
-  return <div>{renderList()}</div>;
-
+    return <ul>
+      {currentPosts.map((post, index) => (
+        <li key={index}>
+          {post.company_name}
+        </li>
+      ))}
+      </ul>
+  };
+  
+// console.log(jobsRequest.length)
+  
+  return (
+    <div>
+        {renderList()}
+    </div>        
+  );
 }
