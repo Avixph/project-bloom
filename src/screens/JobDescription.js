69a24a6edbf2f602c74194ce27073012ed6e932d
Quiana Berry <berry.quiana@gmail.com>
2021-03-15 21:49:37 -0400

    notes

--- src/screens/JobDescription.js
+++ src/screens/JobDescription.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 //step 1 - see data shows in console
 //step 2 - pull API data through to show on Job dEscription screens?
 //step 3 - use interpolation so user can access specifically what they want
+//
 
 export default function JobDescription() {
   const [jobsArray, setJobsArray] = useState([]);
