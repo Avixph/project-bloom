03e802516dd6bc16be5b885c95ed39c0d25994f3
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-03-19 01:11:04 -0400

    editted some code

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import RandomJobList from "../components/RandomJobList";
 import Footer from "../components/Footer";
 
-export default function HomePage({ lightSwitch, light }) {
+export default function HomePage({ lightSwitch, dark }) {
   return (
     <div className="home">
       <Header handleLights={lightSwitch} dark={dark} />
