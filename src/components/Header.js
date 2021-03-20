834700c67d611012df6f37b9e5c8a97750833b7a
david espinal <despinal0425@gmail.com>
2021-03-19 23:32:43 -0400

    added the hamburger component to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import bloomLogoLight from "../images/logos/project-bloom-logo-light.svg";
 import bloomLogoDark from "../images/logos/project-bloom-logo-dark.svg";
 import LightsOut from "../components/LightsOut";
-
+import Hamburger from "./Humburger";
 
 export default function Header({ handleLights, dark}) {
 
@@ -11,28 +11,27 @@ export default function Header({ handleLights, dark}) {
     <header className={dark ? "headerArea" : "darkHeaderArea"}>
       <div className={dark ? "headerContent" : "darkHeaderContent"}>
         <img src={dark ? bloomLogoLight : bloomLogoDark} alt="" className="logo" />
-
         <nav>
           <ul>
-            <li>
+            <li className="displayNav">
               <NavLink exact to={"/home"}>
                 Home
               </NavLink>
             </li>
-            <li>
+            <li className="displayNav">
               <NavLink exact to={"/search"}>
                 Search Jobs
               </NavLink>
             </li>
-            <li>
+            <li className="displayNav">
               <NavLink exact to={"/about"}>
                 About
               </NavLink>
             </li>
-            {/* <li>
-              <Hamburger />
-            </li> */}
-            <li>
+            <li className="displayHamburger">
+              <Hamburger dark={dark} />
+            </li>
+            <li className="displayNav">
               <LightsOut lightSwitch={handleLights} />
             </li>
           </ul>
